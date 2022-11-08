aafdb714a1e505395b84f4d5e2a18d523578c8ec
raheemadamboev <vividricemaryandmax@gmail.com>
2022-11-08 21:30:45 +0500

    add location service

---
+++ app/src/main/java/xyz/teamgravity/backgroundlocationtracker/core/service/LocationService.kt
@@ -0,0 +1,67 @@
+package xyz.teamgravity.backgroundlocationtracker.core.service
+
+import android.content.Intent
+import androidx.lifecycle.LifecycleService
+import androidx.lifecycle.lifecycleScope
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import xyz.teamgravity.backgroundlocationtracker.core.notification.LocationNotification
+import xyz.teamgravity.backgroundlocationtracker.core.util.LocationProvider
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class LocationService : LifecycleService() {
+
+    companion object {
+        const val ACTION_START = "actionStart"
+        const val ACTION_STOP = "actionStop"
+        const val INTERVAL = 10_000L
+    }
+
+    @Inject
+    lateinit var notification: LocationNotification
+
+    @Inject
+    lateinit var location: LocationProvider
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        when (intent?.action) {
+            ACTION_START -> start()
+            ACTION_STOP -> stop()
+        }
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    private fun start() {
+        observe()
+        startForeground()
+    }
+
+    private fun stop() {
+        stopForeground(STOP_FOREGROUND_REMOVE)
+        stopSelf()
+    }
+
+    private fun observe() {
+        observeLocation()
+    }
+
+    private fun startForeground() {
+        startForeground(LocationNotification.ID, notification.createNotification())
+    }
+
+    private fun observeLocation() {
+        lifecycleScope.launch {
+            location.getLocation(INTERVAL)
+                .catch { it.printStackTrace() }
+                .collectLatest { location ->
+                    notification.updateContentText(
+                        latitude = location.latitude,
+                        longitude = location.longitude
+                    )
+                }
+        }
+    }
+}
